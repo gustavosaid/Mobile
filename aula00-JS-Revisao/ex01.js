05f1909422c7e410559f502a8e488ca1936f6b24
gustavosaid <104603397+gustavosaid@users.noreply.github.com>
2025-02-08 00:25:22 +0000

    codigo do dia 07-02

---
+++ aula00-JS-Revisao/ex01.js
@@ -0,0 +1,11 @@
+// Exemplo 1: Variaveis (var, let e const)
+
+// usando var (escopo global/função)
+var nome = "Gustavo"
+
+if(true){
+    let nome = "Maria"
+    console.log(nome); //Maria
+}
+
+console.log(nome)
