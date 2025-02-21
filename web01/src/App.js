12cd02fcf8ac78cec8e8841af3376dc9b9ebf2a6
gustavosaid <104603397+gustavosaid@users.noreply.github.com>
2025-02-21 22:25:40 +0000

    codigo das aulas do dia 21-02 REACT

---
+++ web01/src/App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+
+const nome = "Gustavo";
+
+const elemento = (
+  <div>
+    <h1> Ola, {nome} !</h1>
+    <p>Bem vindo ao ultimo ano do seu curso !</p>
+  </div>
+);
+
+function App(){
+  return elemento;
+}
+
+export default App;
