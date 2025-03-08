4670d3d67accfab07aeefbb8c78be9c5572f03be
gustavosaid <104603397+gustavosaid@users.noreply.github.com>
2025-03-08 00:26:14 +0000

    codigos do dia 07/03

---
+++ ex04/App.js
@@ -0,0 +1,60 @@
+
+import React, {useState} from "react";
+import { KeyboardAvoidingView,
+  ScrollView,
+  TextInput,
+  Text,
+  Button,
+  StyleSheet,
+  View,
+  } from "react-native";
+
+export default function App() {
+  
+    const [input,setInput] = useState('');
+
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Text style={styles.title}> Formulário</Text>
+          {Array.from({length:10 }).map((_, index) => (
+            <TextInput
+              key={index}
+              style={styles.input}
+              placeholder={`Campo ${index + 1}`}
+              value={input}
+              onChangeText={setInput}
+            />
+        ))}
+
+        <Button title="Enviar" onPress={() => alert('Dados enviados !')}/>
+        </ScrollView>
+      </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex:1,
+
+  },
+  scrollContainer:{
+    padding:20,
+
+  },
+  title:{
+    marginTop:20,
+    fontSize:24,
+    fontWeight:'bold',
+    marginBottom:20,
+    textAlign:'center'
+  },
+  input:{
+    marginBottom:15,
+    padding: 10,
+    borderWidth:1,
+    borderColor:'#ccc',
+    borderRadius:8,
+
+  },
+});
